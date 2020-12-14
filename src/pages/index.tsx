1f4cf0f2bb1b94966f6cb040cb0f470fde3b6417
brandaoplaster <brandaoplaster@gmail.com>
2020-12-13 21:29:44 -0300

    style: adding eslint and prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+    </Head>
+
+    <main>
+      <h1>Welcome to</h1>
+    </main>
+  </div>
+);
+
+export default Home;
