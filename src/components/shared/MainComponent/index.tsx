0131d0198f3ac1761b2a21ac0c4d668223a2e22a
brandaoplaster <brandaoplaster@gmail.com>
2020-12-15 23:25:57 -0300

    chore: [WIP] create component basic

--- src/components/shared/MainComponent/index.tsx
+++ src/components/shared/MainComponent/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from '../Header/StorefrontHeader';
 
 const MainComponent: React.FC = ({ children }) => {
   return (
-    <div className="d-flex flex-column sticky-footer-wrapper">{children}</div>
+    <div className="d-flex flex-column sticky-footer-wrapper">
+      <Header />
+
+      <div className="container flex-fill">{children}</div>
+    </div>
   );
 };
 
