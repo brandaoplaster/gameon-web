d1b86270c24e6b15e3f79b15dba86fc0cb3b55f8
brandaoplaster <brandaoplaster@gmail.com>
2021-01-05 22:06:48 -0300

    chore: adding redux to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
-      <Head>
-        <title>Game On</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <div>
+          <Head>
+            <title>OneBitGames</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
 
-      <Component {...pageProps} />
-    </>
+          <Component {...pageProps} />
+        </div>
+      </PersistGate>
+    </Provider>
   )
 };
 
