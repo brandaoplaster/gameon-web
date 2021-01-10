bd073a035b27e90529123fe3a73aa3494c0402ef
brandaoplaster <brandaoplaster@gmail.com>
2021-01-10 17:46:26 -0300

    chore: adding lib toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,13 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { toast } from 'react-toastify';
 import { store, persistor } from '../store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure();
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
